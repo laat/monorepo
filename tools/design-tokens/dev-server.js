09901101ca2e00a3550fcfc67db140fe51596249
Sigurd Fosseng <sigurd@fosseng.net>
2018-05-01 20:26:03 +0200

    clean up dev-server

--- tools/design-tokens/dev-server.js
+++ tools/design-tokens/dev-server.js
@@ -1,33 +1,34 @@
+const opn = require("opn");
 const reload = require("reload");
 const onKill = require("death");
 const readline = require("readline");
 const express = require("express");
 
-console.log("hello!!!");
-
 const app = express();
 const reloadServer = reload(app);
-const publicPath = process.argv[2];
 
-app.use(express.static(publicPath, { index: process.argv[3] }));
+app.use(express.static(process.argv[2], { index: process.argv[3] }));
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: true
+const server = app.listen(0, () => {
+  const url = `http://localhost:${server.address().port}`;
+  opn(`http://localhost:${server.address().port}`);
+  console.log(`Dev Server started at ${url}`);
 });
 
-rl.on("line", line => {
-  if (line.includes("IBAZEL_BUILD_COMPLETED") && line.includes("SUCCESS")) {
-    console.log("DevServer Got " + line);
-    console.log("DevServer reloading");
-    reloadServer.reload();
-  }
-});
+readline
+  .createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: true
+  })
+  .on("line", line => {
+    if (line.includes("IBAZEL_BUILD_COMPLETED") && line.includes("SUCCESS")) {
+      console.log("DevServer Got " + line);
+      console.log("DevServer reloading");
+      reloadServer.reload();
+    }
+  });
 
 onKill(() => {
   process.exit();
 });
-
-const port = 5000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
